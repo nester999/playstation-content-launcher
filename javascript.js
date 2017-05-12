146d7addb8205874386edbd7cc8ef78f66ad2088
DUARTE, EDAN S <ed736u@att.com>
2017-05-12 08:58:39 -0700

    Final test cleanup

--- javascript.js
+++ javascript.js
@@ -2,68 +2,65 @@ $(document).ready( function() {
   var numContentLauncherObjects = $('.content-container').length-1;
   var contentLauncherCarouselIndex = 1;
   var numNavbarObjects = $('.nav-content-container').length-1;
-  console.log('numNavbarObjects: ', numNavbarObjects);
   var navbarCarouselIndex = 0;
-
+  var carouselSlideAmount = 145;
   
-  
-  console.log('contentLauncherCarouselIndex: ', contentLauncherCarouselIndex);
-  $('.content-container').eq(contentLauncherCarouselIndex).focus();
+  $('.content-container').eq(contentLauncherCarouselIndex).focus(); // initial focus of element
 
-  $( ".content-container" ).on('keydown', function(event) {
-    console.log('event: ', event.target);
-    event.preventDefault();
+  $( ".content-container" ).on('keydown', function(event) { // key events for content container
+    event.preventDefault(); // keep window from scrolling
     switch(event.keyCode) {
       case(13): // ENTER
+      case(40): // v
         $('.actual-content-section').addClass('actual-content-focused');
         $('.menus-container').addClass('actual-content-focused');
-        $('li.actual-content-container').focus();
+        setTimeout(() => {
+          $('li.actual-content-container').focus();
+        }, 300);
         break;
       case(37): // <-
         if(contentLauncherCarouselIndex > 0) {
           contentLauncherCarouselIndex--;
-          console.log('contentLauncherCarouselIndex <-', contentLauncherCarouselIndex);
           $('.content-container').eq(contentLauncherCarouselIndex).focus();
         } 
         break;
       case(38): // ^
-        console.log('going up: ', navbarCarouselIndex);
         $('.menus-container').addClass('navbar-focused');
-        $('.nav-content-container').eq(navbarCarouselIndex).focus();
+        $('.actual-content-section').addClass('navbar-focused');
+        setTimeout(() => {
+          $('.nav-content-container').eq(navbarCarouselIndex).focus();
+        }, 100);
         break;
       case(39): // ->
         if(contentLauncherCarouselIndex < numContentLauncherObjects) {
           contentLauncherCarouselIndex++;
-          console.log('contentLauncherCarouselIndex ->', contentLauncherCarouselIndex);
           $('.content-container').eq(contentLauncherCarouselIndex).focus();
         }
-        
         break;
       default:
         break;
     }
     if(contentLauncherCarouselIndex !== 0)
-    $('.content-launcher-container').css("transform","translate3d(" + ((contentLauncherCarouselIndex-1) * -145 ) + "px, 0px, 0px)");
+      $('.content-launcher-container').css("transform","translate3d(" + ((contentLauncherCarouselIndex-1) * -carouselSlideAmount ) + "px, 0px, 0px)");
   });
   
     $( ".nav-content-container" ).on('keydown', function(event) {
-      event.preventDefault();
+      event.preventDefault(); // keep window from scrolling
       switch(event.keyCode) {
         case(37): // <-
           if(navbarCarouselIndex > 0) {
             navbarCarouselIndex--;
-            console.log('navbarCarouselIndex <-', navbarCarouselIndex);
             $('.nav-content-container').eq(navbarCarouselIndex).focus();
           }
           break;
         case(40): // v
-           $('.menus-container').removeClass('navbar-focused');
+          $('.menus-container').removeClass('navbar-focused');
+          $('.actual-content-section').removeClass('navbar-focused');
           $('.content-container').eq(contentLauncherCarouselIndex).focus();
           break;
         case(39): // ->
           if(navbarCarouselIndex < numNavbarObjects) {
             navbarCarouselIndex++;
-            console.log('navbarCarouselIndex ->', navbarCarouselIndex);
             $('.nav-content-container').eq(navbarCarouselIndex).focus();
           }
           break;
@@ -72,24 +69,20 @@ $(document).ready( function() {
       }
   });
   
-    $( ".actual-content-container" ).on('keydown', function(event) {
-    console.log('event: ', event.target);
+  $( ".actual-content-container" ).on('keydown', function(event) { // actual content keyboard function
     event.preventDefault();
     switch(event.keyCode) {
       case(38): // ^
         $('.actual-content-section').removeClass('actual-content-focused');
         $('.menus-container').removeClass('actual-content-focused');
-        $('.content-container').eq(contentLauncherCarouselIndex).focus();
+        setTimeout(() => {
+          $('.content-container').eq(contentLauncherCarouselIndex).focus();
+        }, 100);
         break;
       default:
         break;
     }
-    if(contentLauncherCarouselIndex !== 0)
+    if(contentLauncherCarouselIndex !== 0) // slide carousel if not at the end
     $('.content-launcher-container').css("transform","translate3d(" + ((contentLauncherCarouselIndex-1) * -145 ) + "px, 0px, 0px)");
   });
-
-  $(".content-container").on('focus', function() {
-    
-  });
-  
 });
